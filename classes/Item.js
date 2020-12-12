28153602c1afd4d2cf090f2174fbd103375d8268
Jade Allen Cook <jadeallencook@gmail.com>
2020-12-11 20:17:29 -0600

    Reformatted item class

--- classes/Item.js
+++ classes/Item.js
@@ -1,11 +1,11 @@
 class Item {
-    constructor({ id, title, price, description, photos  }) {
-        this.id = id || 0;
-        this.title = title || '';
-        this.price = price || 0;
-        this.description = description || '';
-        this.photos = photos || [];
-    }
+  constructor({ id, title, price, description, photos }) {
+    this.id = id || 0;
+    this.title = title || '';
+    this.price = price || 0;
+    this.description = description || '';
+    this.photos = photos || [];
+  }
 }
 
 export default Item;
