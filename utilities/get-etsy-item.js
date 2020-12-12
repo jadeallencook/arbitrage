141cd8f79b8a3800d76868a5b20b4b3ca1ebdac7
Jade Allen Cook <jadeallencook@gmail.com>
2020-12-11 20:16:49 -0600

    Added logic for getting Etsy item

---
+++ utilities/get-etsy-item.js
@@ -0,0 +1,16 @@
+const getEtsyItem = () => {
+  const title = document.querySelector('h1').innerText;
+  const id = Number(window.location.href.split('/')[4]);
+  const price = Number(
+    document
+      .querySelector('p.wt-text-title-03.wt-mr-xs-2')
+      .innerText.split('\n')[1]
+      .substring(1)
+  );
+  const description = document.querySelector(
+    '#wt-content-toggle-product-details-read-more > p'
+  ).innerText;
+  return { title, id, price, description };
+};
+
+export default getEtsyItem;
